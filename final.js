39903bf7b4af86b9e19410ee8f482b9372672b2b
salonisatoru <salonipatwal17@gmail.com>
2024-09-26 12:25:06 +0530

    Create final.js

---
+++ final.js
@@ -0,0 +1,76 @@
+
+
+//step 1: get DOM
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+
+nextDom.onclick = function(){
+    showSlider('next');    
+}
+
+prevDom.onclick = function(){
+    showSlider('prev');    
+}
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    next.click();
+}, timeAutoNext)
+function showSlider(type){
+    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+    
+    if(type === 'next'){
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
+    }else{
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        next.click();
+    }, timeAutoNext)
+}
+
+
+// Get references to the button and form
+const button = document.getElementById('button');
+const myForm = document.getElementById('myForm');
+
+// Add an event listener to the button to handle the click
+button.addEventListener('click', function() {
+    // Toggle the visibility of the form
+    if (myForm.style.display === 'none' || myForm.style.display === '') {
+        myForm.style.display = 'block';  // Show the form
+    } else {
+        myForm.style.display = 'none';  // Hide the form
+    }
+});
+
+/*form*/
+document.getElementById('formbtn').addEventListener('click',function(){
+    document.getElementById('myform').style.display= 'block';
+});
+
+document.getElementById('closeformbtn').addEventListener('click',function(){
+    document.getElementById('myform').style.display= 'block';
+});
+
